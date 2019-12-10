69fa0a220f22fc209168fb2f25cce6f680c82fb3
Giang Nguyen <hoangtruonggiangnguyen@gmail.com>
2019-12-10 12:12:54 +0700

    add authentication feature to api

---
+++ code/Section 4/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request
+from flask_restful import Resource, API, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = "Giang"
+api = API(app)
+jwt = JWT(app, authenticate, indentity)  # /auth
+
+items = []
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannot be left blank!")
+
+    @jwt_required
+    def get(self, name):
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        return {"item": item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x["name"] == name, items), None):
+            return {"message": "An item with name {} already exists".format(name)}, 400
+
+        data = Item.parser.parse_args()
+
+        item = {"name": data["name"], "price": data["price"]}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x["name"] != name, items))
+
+        return {"message": "Item deleted"}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        if item is None:
+            item = {"name": data["name"], "price": data["price"]}
+            items.append(item)
+        else:
+            items.update(data)
+
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}, 200
+
+
+api.add_resrouce(Item, "/item<string:name>")
+api.add_resrouce(ItemList, "/items")
+
+app.run(port=5000)
